78bd16e637a83d20a201e5440672da463b434932
napadsakorn42 <ploynapadsakorn@gmail.com>
2022-04-25 21:56:48 +0700

    implement api get account by id

--- backend/account.js
+++ backend/account.js
@@ -7,4 +7,10 @@ router.get("/accounts", async (req, res) => {
   res.status(200).json(accounts.account);
 });
 
+router.get("/account/:id", async (req, res) => {
+  const { id } = req.params;
+  const response = accounts.account.find((item) => item.id === +id);
+  res.status(200).json(response);
+});
+
 module.exports = router;
